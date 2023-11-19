88182c2e8d77455839b62268f3826734b823e8bf
Thiago Sousa <thiagosousa81@gmail.com>
2023-11-19 00:18:47 -0300

    §

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in 192.168.100.{1..254}; do
+        ping -c1 $i >> /dev/null
+
+        if [ $? = 0 ]; then 
+                echo "HOST ATIVO -> $i"
+                nmap -sV -p "*"  $i 
+
+        else 
+                echo "HOST INATIVO -> $i"
+        fi
+done
