b4b64ec995356207da933e78e4a2c3abd8dd27a3
Thiago Sousa <thiagosousa81@gmail.com>
2023-11-19 17:14:23 -0300

    §

--- writeOutput.sh
+++ writeOutput.sh
@@ -8,3 +8,7 @@ touch /tmp/teste.txt
 tail -f /tmp/teste.txt
 
 # Se outro terminal for aberto e executado o comando "echo Texto >> /tmp/teste.txt" o texto do arquivo é anexado no outro terminal
+# É possível colocar variáveis tbm
+# Execute isso em outro terminal para entender
+# NOME="Thiago Sousa"
+# echo $NOME >> /tmp/teste.txt
