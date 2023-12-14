bfe9aeabc8e8c5c6b705bc8d8313be32a635b04b
Thiago Sousa <thiagosousa81@gmail.com>
2023-12-14 11:37:46 -0300

    §

---
+++ ipconfigs.sh
@@ -0,0 +1,7 @@
+# É possível alterar as configurações do IP estático do Kali por meio do arquivo
+
+vim /etc/network/interfaces
+
+# As configurações de DNS do Kali ficam no arquivo abaixo
+
+vim /etc/hosts
