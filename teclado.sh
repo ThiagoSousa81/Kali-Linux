9b43bfad816d2c66c5010f4824f5c4e17b56ef8a
Thiago Sousa <thiagosousa81@gmail.com>
2024-07-22 10:56:06 -0300

    §

---
+++ teclado.sh
@@ -0,0 +1,11 @@
+# Sempre definir essas configurações de teclado para o /etc/default/keyboard 
+# KEYBOARD CONFIGURATION FILE
+
+# Consult the keyboard(5) manual page.
+
+XKBMODEL="pc105"
+XKBLAYOUT="br"
+XKBVARIANT=""
+XKBOPTIONS="model:abnt2,layout:br"
+
+BACKSPACE="guess"
