d3bbb5d8866ae774a65926114a790a1302503fd5
ThiagoSousa81 <thiagosousa81@gmail.com>
2025-02-04 08:38:11 -0300

    § wifite §

---
+++ WiFi-Cracks/wifite.sh
@@ -0,0 +1,9 @@
+# Esse programa serve para testar vulnerabilidades em sua rede
+# Você é responsável por suas ações, portanto use com cautela!!!
+
+# Entramos em modo sudo
+sudo su
+
+# E abrimos o wifite
+
+wifite
